640748890f0db21048ca605e84b9e529642b68b3
Kevin Liao <37547030+liao1k@users.noreply.github.com>
2025-09-12 08:30:34 -0400

    Fix AWS URL typo, missing a '

--- streamlit_video_stream.py
+++ streamlit_video_stream.py
@@ -13,7 +13,7 @@ with tab3:
     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 with tab4:
     st.header('AWS 财金视频')
-    st.video('https://banyanhill.s3.dualstack.us-east-1.amazonaws.com/StrategicFortunes/Webinar/2025/Landing_Pages/090225_IKA_LP.html?vid2=75525ca09396c0ad367ebb455ae64960daf5aab968bdd0b8dfacc5d5bfa87d92f7df0e92a0a2854d1ed2c08675657a64&utm_campaign=090225_IKA_Weekly_-_All&utm_source=Iterable&utm_medium=email&itbl_templateId=19310838&itbl_campaignId=14820195
-)
+    st.video('https://banyanhill.s3.dualstack.us-east-1.amazonaws.com/StrategicFortunes/Webinar/2025/Landing_Pages/090225_IKA_LP.html?vid2=75525ca09396c0ad367ebb455ae64960daf5aab968bdd0b8dfacc5d5bfa87d92f7df0e92a0a2854d1ed2c08675657a64&utm_campaign=090225_IKA_Weekly_-_All&utm_source=Iterable&utm_medium=email&itbl_templateId=19310838&itbl_campaignId=14820195')
+
 
 
