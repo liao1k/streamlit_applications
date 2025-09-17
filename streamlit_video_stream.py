c1f25f6c392a45d14c83e778d903ab419bbd6dc9
Kevin Liao <liao1k@gmail.com>
2025-09-17 09:01:45 -0400

    Add 远程视频

--- streamlit_video_stream.py
+++ streamlit_video_stream.py
@@ -15,17 +15,17 @@ try:
 except Exception as e:
     HEIF_ERROR = str(e)
 st.title('昆明 Streamlit on GitHub via Cursor AI Editor')
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["一只猫", "一只狗", "一只猫头鹰", "当前市场", "本地视频"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["一只猫", "一只狗", "一只猫头鹰", "当前市场", "本地视频", "远程视频"])
 
 with tab1:
     st.header("一只猫")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    st.image("https://static.streamlit.io/examples/cat.jpg")
 with tab2:
     st.header("一只狗")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.image("https://static.streamlit.io/examples/dog.jpg")
 with tab3:
     st.header("一只猫头鹰")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.image("https://static.streamlit.io/examples/owl.jpg")
 with tab4:
     st.header("当前市场")
     if HEIF_SUPPORTED:
@@ -39,6 +39,9 @@ with tab4:
 with tab5:
     st.header('本地视频')
     st.video("IMG_1908.MOV")
+with tab6:
+    st.header('远程视频')
+    st.video("https://youtu.be/Oe59swhW4p0")
 
 
 
